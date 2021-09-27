cbb55cceea5d01fec1dedcfe40045c08bec0b377
Ibidapo-chad <Ibidapo.chad@gmail.com>
2021-09-27 02:19:58 +0000

    files added

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  const myAmenities = {};
+  $('input[type=checkbox]').change(function() {
+    if (this.checked) {
+      myAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete myAmenities[$(this).attr('data-id')];
+    }
+    const amenityList = Object.values(myAmenities);
+    if (amenityList.length > 0) {
+      $('.amenities > h4').text(amenityList.join(', '));
+    } else {
+      $('.amenities > h4').html('&nbsp;');
+    }
+  });
+});
+
+$(function() {
+  $.ajax({
+    type: 'GET',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    success: (data) => {
+      console.log(data);
+      if (data.status === "OK") {
+        console.log(data);
+        $("div#api_status").addClass("available");
+      }
+    }
+  });
+});
